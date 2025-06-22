cf6a44c93595cdd4967012eb0e466bf3221e3965
BABUL X QUEEN <biswajitdasyt85@gmail.com>
2025-06-22 22:25:34 +0530

    Update __main__.py

--- BrandrdXMusic/__main__.py
+++ BrandrdXMusic/__main__.py
@@ -50,7 +50,7 @@ async def init():
         pass
     await Hotty.decorators()
     LOGGER("BrandrdXMusic").info(
-        "ᴅʀᴏᴘ ʏᴏᴜʀ ɢɪʀʟꜰʀɪᴇɴᴅ'ꜱ ɴᴜᴍʙᴇʀ ᴀᴛ @BRANDED_PAID_CC ᴊᴏɪɴ @BRANDRD_BOT , @BRANDED_WORLD ꜰᴏʀ ᴀɴʏ ɪꜱꜱᴜᴇꜱ"
+        "ᴅʀᴏᴘ ʏᴏᴜʀ ɢɪʀʟꜰʀɪᴇɴᴅ'ꜱ ɴᴜᴍʙᴇʀ ᴀᴛ @mr_babul0 ᴊᴏɪɴ @BABUL_X_MUSIC_BOT , @BABUL_ON_FIRE ꜰᴏʀ ᴀɴʏ ɪꜱꜱᴜᴇꜱ"
     )
     await idle()
     await app.stop()
